43a50ba1e2d0c0e11b546eb81ad19392abf00e2b
sean <seathony@gmail.com>
2016-02-02 10:55:29 -0500

    hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-const path = require('path')
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+const common  = {
  // Entry accepts a path or an object of object of entries.
  // The buld chapter contains an example of the latter 
   entry: PATHS.app,
@@ -14,4 +17,35 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
-  
+
+// Default configuration
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Displays only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT,
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET == 'build') {
+  module.exports = merge(common, {});
+}
